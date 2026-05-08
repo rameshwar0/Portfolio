b76f6044988d4e88ad989411e17f7725dc2e3f9b
rameshwar0 <rameshwar.ghanekar05@gmail.com>
2026-05-08 15:43:12 +0530

    Fix Cloudflare Workers deployment configuration.

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-<<<<<<< Updated upstream
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [tailwindcss(), react(), cloudflare()],
 })
-=======
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-})
->>>>>>> Stashed changes
